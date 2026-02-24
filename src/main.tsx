3d36cd603bd79fea45cd50f74e49b340bbc7581d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-02-24 20:54:55 +0200

    Install modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
+import "modern-normalize";
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
